76e121f3a09dd013e44a80f996530814c71c5f13
Pyegor <65090191+Pyegor@users.noreply.github.com>
2020-08-14 11:43:06 +0500

    Update BotFirst.py

--- BotFirst.py
+++ BotFirst.py
@@ -2,11 +2,10 @@ from telegram.ext import Updater, CommandHandler
 
 
 def hello(update, context):
-    update.message.reply_text(
-        '{}'.format(update.message.text))
+    update.message.reply_text('{}'.format(update.message.text))
 
 
-updater = Updater('1356334063:AAGRcIl8hOn6He6f0pCZR_qG8dGVKFb6l_E', use_context=True)
+updater = Updater('YOUR_TOKEN', use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('12_21', hello))
 
